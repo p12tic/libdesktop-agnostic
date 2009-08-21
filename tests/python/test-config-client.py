fa5ddcd0bae7cde9ca8d52e696fe627829412d74
Mark Lee <bzr@lazymalevolence.com>
2009-08-21 15:27:35 -0700

    Tests (Python Config Client): refactor into a unittest TestCase.

--- tests/python/test-config-client.py
+++ tests/python/test-config-client.py
@@ -17,18 +17,62 @@
 
 import glib
 import time
+import unittest
 from desktopagnostic import config
 
+'''
+Map format: type name => (group, key, default, set value)
+'''
+type_key_map = {
+    'bool': ('numeric', 'boolean', True, False),
+    'int': ('numeric', 'integer', 3, 10),
+    'float': ('numeric', 'float', 3.14, 2.718),
+    'string': ('misc', 'string', 'Foo bar', 'Quux baz')
+    }
 
-class TestCase:
+'''
+Map format: type name => (default, set value)
+'''
+list_value_map = {
+    'bool': ([True, False], [False, True, False]),
+    'int': ([1, 2, 3], [10, 20, 30]),
+    'float': ([1.618, 2.718, 3.141], [10.5, 20.6, 30.7]),
+    'string': (['foo', 'bar'], ['Quux', 'Baz', 'Foo'])
+    }
 
-    def __init__(self):
+def create_type_tests(type_name, group, key, default, set_val):
+    list_default, list_set_val = list_value_map[type_name]
+    def test_default(self):
+        self.check_values(group, key, default, type_name)
+    def test_default_list(self):
+        self.check_values('list', key, list_default, 'list')
+    def test_set(self):
+        self.check_set_values(group, key, set_val, type_name, False)
+    def test_set_list(self):
+        self.check_set_values('list', key, list_set_val, 'list', False)
+    def test_set_value(self):
+        self.check_set_values(group, key, set_val, type_name, True)
+    def test_set_list_value(self):
+        self.check_set_values('list', key, list_set_val, 'list', True)
+    return {
+        'test_default_%s' % type_name: test_default,
+        'test_default_list_%s' % type_name: test_default_list,
+        'test_set_%s' % type_name: test_set,
+        'test_set_list_%s' % type_name: test_set_list,
+        'test_set_value_%s' % type_name: test_set_value,
+        'test_set_list_value_%s' % type_name: test_set_list_value}
+
+
+class TestConfigClient(unittest.TestCase):
+
+    def setUp(self):
         self.ml = glib.MainLoop()
         self.notify_counter = 0
         self.client = config.Client('../test-config.schema-ini')
         self.client.reset(False)
 
-    def check_values(self, group, key, expected, get_func):
+    def check_values(self, group, key, expected, type_name):
+        get_func = getattr(self.client, 'get_%s' % type_name)
         actual = get_func(group, key)
         actual_value = self.client.get_value(group, key)
         if isinstance(expected, float):
@@ -40,49 +84,17 @@ class TestCase:
             actual = [round(x, 3) for x in actual]
             actual_value = [round(x, 3) for x in actual_value]
         ERROR_MSG = 'Fail! Expected value: %s, actual: %s'
-        assert expected == actual, ERROR_MSG % (str(expected), str(actual))
-        assert actual == actual_value
-
-    def check_values_list(self, group, key, expected):
-        self.check_values(group, key, expected, self.client.get_list)
-
-    def test_defaults(self):
-        self.check_values('numeric', 'boolean', True, self.client.get_bool)
-        self.check_values('numeric', 'integer', 3, self.client.get_int)
-        self.check_values('numeric', 'float', 3.14, self.client.get_float)
-        self.check_values('misc', 'string', 'Foo bar', self.client.get_string)
-        self.check_values_list('list', 'boolean', [True, False])
-        self.check_values_list('list', 'integer', [1, 2, 3])
-        self.check_values_list('list', 'float', [1.618, 2.718, 3.141])
-        self.check_values_list('list', 'string', ['foo', 'bar'])
+        self.assertEqual(expected, actual,
+                         ERROR_MSG % (str(expected), str(actual)))
+        self.assertEqual(actual, actual_value)
 
     def check_set_values(self, group, key, expected, type_name, set_value):
-        get_func = getattr(self.client, 'get_%s' % type_name)
         if set_value:
             set_func = self.client.set_value
         else:
             set_func = getattr(self.client, 'set_%s' % type_name)
         set_func(group, key, expected)
-        self.check_values(group, key, expected, get_func)
-
-    def check_set_all_types(self, set_value):
-        self.check_set_values('numeric', 'boolean', False, 'bool', set_value)
-        self.check_set_values('numeric', 'integer', 10, 'int', set_value)
-        self.check_set_values('numeric', 'float', 2.718, 'float', set_value)
-        self.check_set_values('misc', 'string', 'Quux baz', 'string', set_value)
-        expected_lists = [
-            ([False, True, False], 'boolean'),
-            ([10, 20, 30], 'integer'),
-            ([10.5, 20.6, 30.7], 'float'),
-            (['Quux', 'Baz', 'Foo'], 'string')]
-        for expected, key in expected_lists:
-            self.check_set_values('list', key, expected, 'list', set_value)
-
-    def test_set(self):
-        self.check_set_all_types(False)
-        self.client.reset(False)
-        self.test_defaults()
-        self.check_set_all_types(True)
+        self.check_values(group, key, expected, type_name)
 
     def string_changed(self, group, key, value):
         self.notify_counter += 1
@@ -97,7 +109,7 @@ class TestCase:
         time.sleep(0.25)
         while ctx.pending():
             ctx.iteration()
-        assert self.notify_counter == expected_counter
+        self.assertEqual(self.notify_counter, expected_counter)
 
     def test_notify(self):
         ctx = self.ml.get_context()
@@ -115,8 +127,11 @@ class TestCase:
         self.client.notify_remove('misc', 'string', self.string_changed2)
         self.check_notify(ctx, 'Baz foo', 8)
 
+# add the type-specific tests to the testcase
+for type_name, data in type_key_map.iteritems():
+    methods = create_type_tests(type_name, *data)
+    for name, method in methods.iteritems():
+        setattr(TestConfigClient, name, method)
+
 if __name__ == '__main__':
-    t = TestCase()
-    t.test_defaults()
-    t.test_set()
-    t.test_notify()
+    unittest.main()
